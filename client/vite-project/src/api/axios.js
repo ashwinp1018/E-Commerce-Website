d240df82289176b307b2ba72047368c0b3fa4a42
Ashwin Parashar <133157509+ashwinp1018@users.noreply.github.com>
2025-07-28 12:41:49 +0530

    Update axios.js

--- client/vite-project/src/api/axios.js
+++ client/vite-project/src/api/axios.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api', 
+  baseURL: import.meta.env.VITE_API_URL || "https://e-commerce-website-1-k4j8.onrender.com/api",
 });
 
 export default API;
